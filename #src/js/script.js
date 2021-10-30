4acf180a03d9583b3d54415959819bdc302b3914
revveerie <revveerie@gmail.com>
2021-10-31 01:42:11 +0300

    feat: add project popups

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,15 +1,3 @@
 @@include('scripts/menu.js')
 @@include('scripts/preloader.js')
-
-
-
-
-
-
-
-
-
-
-
-
-
+@@include('scripts/popup.js')
--- #src/js/scripts/menu.js
+++ #src/js/scripts/menu.js
@@ -5,9 +5,20 @@ let iconMenu = document.querySelector('.icon-menu'),
 
 iconMenu.onclick = function() {
 	iconMenu.classList.toggle('_menu-active');
-	menuBody.classList.toggle('_menu-active');
-	page.classList.toggle('_lock');
+	
 	body.classList.toggle('_lock');
+	if (menuBody.classList.contains('_menu-active')) {
+		menuBody.classList.toggle('_menu-active');
+		setTimeout(() => {
+			page.classList.toggle('_lock');
+		}, 2000);
+	}
+	else {
+		page.classList.toggle('_lock');
+		setTimeout(() => {
+			menuBody.classList.toggle('_menu-active');
+		}, 2000);
+	}
 }
 
 menuLink.forEach(function(item) {
